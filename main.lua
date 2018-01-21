afab85e7c49cb81b5661c85b6e729e69953b747c
RamiLego4Game <ramilego4game@gmail.com>
2018-01-21 16:22:37 +0200

    Now loads the games images.

---
+++ main.lua
@@ -0,0 +1,66 @@
+--LIKO-12 Games Toolchain--
+
+--PICO-8 Palette
+local palette = {
+  {0,0,0,255}, --Black 1
+  {28,43,83,255}, --Dark Blue 2
+  {127,36,84,255}, --Dark Red 3
+  {0,135,81,255}, --Dark Green 4
+  {171,82,54,255}, --Brown 5
+  {96,88,79,255}, --Dark Gray 6
+  {195,195,198,255}, --Gray 7
+  {255,241,233,255}, --White 8
+  {237,27,81,255}, --Red 9
+  {250,162,27,255}, --Orange 10
+  {247,236,47,255}, --Yellow 11
+  {93,187,77,255}, --Green 12
+  {81,166,220,255}, --Blue 13
+  {131,118,156,255}, --Purple 14
+  {241,118,166,255}, --Pink 15
+  {252,204,171,255} --Human Skin 16
+}
+
+function love.run()
+  print("--============================================--")
+  print("--========LIKO-12 Games Toolchain V1.0========--")
+  print("--============================================--")
+  
+  print("")
+  
+  if not love.filesystem.exists("/Games") then
+    print("!!! The games folder doesn't exist !!!\n")
+    return 1
+  end
+  
+  print("# Loading Games Images #\n")
+  
+  local GamesImages = {}
+  
+  local files_list = love.filesystem.getDirectoryItems("/Games/")
+  for k, filename in ipairs(files_list) do
+    local filepath = "/Games/"..filename
+    if love.filesystem.isFile(filepath) then
+      if filename:sub(-4,-1) == ".png" then
+        local gamename = filename:sub(1,-5)
+        local ok, image = pcall(love.image.newImageData,filepath)
+        if not ok then
+          print("!!! Failed to load '"..gamename.."' !!!",image,"\n")
+          return 1
+        end
+        
+        local imageWidth, imageHeight = image:getDimensions()
+        if imageWidth ~= 256 or imageHeight ~= 256 then
+          print("!!! Invalid image size '"..gamename.."' !!!",imageWidth.."x"..imageHeight.."\n")
+          return 1
+        end
+        
+        GamesImages[gamename] = image
+        print("Loaded '"..gamename.."'")
+      end
+    end
+  end
+  
+  print("\n# Scanning Label Images #\n")
+  
+  return 0
+end
